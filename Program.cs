de0154aa51421f92bf28a9522b8504bb9e223a57
Amber <greenliquidlight@gmail.com>
2023-11-29 10:43:43 -0500

    Reset to initial minimal C# cmake project

---
+++ Program.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace CSharpCallbackDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
